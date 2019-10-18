a48f8ddebe8ed75ce0949af414ce3eec6c1ee61c
shirlyfang <shirly.is.here@gmail.com>
2019-10-17 19:48:16 -0700

    Displaying user info on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+include SessionsHelper
   def new
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+    def logged_in?
+        session.has_key? :user
+    end
+
+    def current_user
+        Marshal.load session[:user] if logged_in?
+    end
 end
