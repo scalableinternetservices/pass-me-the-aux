bafdbf36d59e3429faaa5418534e2193cfeca879
= <heysamlee@gmail.com>
2019-11-14 20:34:55 -0800

    added seed creation of 10000

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+x = 0
+while x < 10000
+    User.create(name: "seed", email: "me@email.com", role: 0, id: rand(1000000), image_url: "https://www.askideas.com/media/12/Cute-Baby-Funny-Pig-Picture.jpg")
+    x = x + 1
+end
