a3da28886bd8de5e65804ddb22d7c434f3f5d025
shirlyfang <shirly.is.here@gmail.com>
2019-12-01 16:11:44 -0800

    5 threads 1 worker again

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,11 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-#
-threads 5, 5
-workers 1
+
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
