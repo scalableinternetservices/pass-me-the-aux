af4d890652787546e2a1916d724c6bf2970285e3
shirlyfang <shirly.is.here@gmail.com>
2019-12-01 18:49:09 -0800

    1 worker 5 threads

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@
 # and maximum; this matches the default thread size of Active Record.
 
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
